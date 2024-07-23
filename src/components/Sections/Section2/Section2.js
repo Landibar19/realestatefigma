9e596a397ab75c31d48a61d84b8f70b6dc9dfeb4
unknown <landibar19@gmail.com>
2024-07-23 10:57:42 +0200

    changes

--- src/components/Sections/Section2/Section2.js
+++ src/components/Sections/Section2/Section2.js
@@ -1,31 +1,50 @@
 import React from 'react';
-import { Box, Container, Typography } from '@mui/material'
+import { Box, Container, Typography, useTheme } from '@mui/material';
 import { Section2Content } from '../../SectionsContent/Section2Content';
 import CardSlider from '../../SlideShow/CardSlider';
 
-
-
 export const Section2 = () => {
+    const theme = useTheme();
     const data = Section2Content;
-  return (
-    <Container maxWidth="xxl"
-    sx={{
-        backgroundColor:'rgb(233, 233, 233)',
-        backgroundSize: 'cover',
-        width:'100%',
-        height:'100%', 
-        display:'flex',
-        justifyContent:'center',
-        alignItems:'center',
-        flexDirection:'column',
-        marginTop: 2,
-        borderRadius:'16px',
-        gap:5}}>
-        <Box width='100%'>
-            <Typography sx={{color:'black', fontWeight: 'bold', fontSize:40}}>Homes For You</Typography>
-            <Typography sx={{color: 'black',fontSize: 17, padding: 2}}>Based on your view history</Typography>
-            <CardSlider data={data} />
-        </Box>
-    </Container>
-  )
+    return (
+        <Container maxWidth="xxl"
+        sx={{
+            backgroundColor: 'rgb(233, 233, 233)',
+            backgroundSize: 'cover',
+            width: '100%',
+            height: '100%', 
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'column',
+            marginTop: 2,
+            borderRadius: '16px',
+            gap: 5,
+            [theme.breakpoints.down('sm')]: {
+                marginTop: 1,
+                gap: 3,
+            }
+        }}>
+            <Box width='100%'>
+                <Typography sx={{
+                    color: 'black', 
+                    fontWeight: 'bold', 
+                    fontSize: 40,
+                    [theme.breakpoints.down('sm')]: {
+                        fontSize: 30, 
+                    }
+                }}>Homes For You</Typography>
+                <Typography sx={{
+                    color: 'black',
+                    fontSize: 17, 
+                    padding: 2,
+                    [theme.breakpoints.down('sm')]: {
+                        fontSize: 15, 
+                        padding: 1,
+                    }
+                }}>Based on your view history</Typography>
+                <CardSlider data={data} />
+            </Box>
+        </Container>
+    )
 }
